d6e23f0615e84156e9bdda0b7abfbf92dcf49657
momoclouq <phamhoangminh050899@gmail.com>
2022-04-27 15:48:34 +0700

    Set up redux/toolkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import CustomizePage from './pages/customize/CustomizePage';
 import MainPage from './pages/main/MainPage';
+import store from './redux/store/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/customize" element={<CustomizePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/customize" element={<CustomizePage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
