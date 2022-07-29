9a4727ba6da65f87c48162b9192905b6e9174671
momoclouq <phamhoangminh050899@gmail.com>
2022-07-29 11:44:30 +0700

    Remove tailwind legacy and add shadow-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { render } from 'react-dom';
+import { StyleSheetManager } from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
+const host = document.querySelector("#katalonChatbot");
+
+const shadow = host.attachShadow({ mode: 'open' });
+// create a slot where we will attach the StyleSheetManager
+const styleSlot = document.createElement('section');
+// append the styleSlot inside the shadow
+shadow.appendChild(styleSlot);
+// create the element where we would render our app
+const renderIn = document.createElement('div');
+// append the renderIn element inside the shadow
+shadow.appendChild(renderIn);
+
+render(
+  <StyleSheetManager target={styleSlot}>
     <App />
-  </React.StrictMode>
+  </StyleSheetManager>,
+  renderIn
 );
